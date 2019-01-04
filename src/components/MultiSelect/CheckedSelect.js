bbbd65b3b02dcda970dc63e67d91272f92a8eae7
Daymel <daym3l@nauta.cu>
2019-01-04 16:39:07 -0500

    first commit

---
+++ src/components/MultiSelect/CheckedSelect.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import MultiSelect from 'react-select-checked';
+import ReactSelect from 'react-select';
+
+class CheckedSelect extends Component {
+    state = {
+        options: [{label: 'Chocolate', value: 'chocolate', disabled: true},
+            {label: 'Vanilla', value: 'vanilla'},
+            {label: 'Strawberry', value: 'strawberry'},
+            {label: 'Caramel', value: 'caramel'},],
+        selectedOptions: [{label: 'Vanilla', value: 'vanilla'}]
+    }
+
+    handleOnChange = val => this.setState({selectedOptions: val})
+
+    render() {
+        return <div style={{width: 300}}>
+            <label><h4>MULTISELECT</h4></label>
+            <ReactSelect
+                name="form-field-name"
+                isMulti
+                value={this.state.selectedOptions}
+                options={this.state.options}
+                onChange={this.handleOnChange}/>
+        </div>
+
+    }
+    }
+
+    export default CheckedSelect
